bb16144fdaf131cfb692f26188f525f58c84335e
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2023-09-07 04:43:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,292 @@
+// QUESTION # 01    CHAPTER #01
+
+
+var firstName = "Nehal";
+alert("First Name: " + firstName);
+
+
+// QUESTION # 02 
+
+
+var lastName = "Tariq";
+alert("Last Name: " + lastName);
+
+
+
+// QUESTION # 03
+
+
+var email = "example123@gmail.com"
+alert("Email: " + email);
+
+
+
+
+
+// QUESTION # 04
+
+var phoneNumber = "090078601"
+alert("phone Number" + phoneNumber)
+
+
+
+
+
+
+// QUESTION # 05
+
+
+var password = "**********";
+alert("password" + password)
+
+
+
+
+// QUESTION # 06
+
+alert("You're learning JavaScript!");
+
+
+
+
+
+
+// QUESTION # 07
+
+
+
+var message = "this is my first javascript assignment";
+alert(message);
+
+
+// CHAPTER #01 COMPLETED 
+
+
+// CHAPTER #02 
+
+
+
+// QUESTION #01 
+
+
+var camelCase = "This is a camelCase variable.";
+
+
+
+
+// QUESTION #02
+
+
+var myUndefinedVariable;
+myUndefinedVariable = 
+name = "Nehal";
+age = 100;
+
+message = "My name is " + name + " and I am " + age + " years old.";
+
+
+
+console.log(message);
+
+
+
+
+// QUESTION #03
+
+
+var teamName = "kings";
+alert("Our team name is: " + teamName);
+
+
+
+
+
+
+// QUESTION #04
+
+
+var bestMan = "Charlie";
+bestMan = "im Nehal"; //  new string is bestMan
+
+
+
+// CHAPTER #02 COMPLETED
+
+
+
+// CHAPTER #03 
+
+
+// QUESTION #01
+
+var caseQty;
+
+
+
+// QUESTION #02
+
+caseQty = 144;
+
+
+
+
+// QUESTION #03
+
+
+
+var num = 9; 
+
+
+
+// QUESTION #04
+
+
+var firstNumber = 80;
+var secondNumber = 60;
+var sum = firstNumber + secondNumber;
+sum=140
+
+
+
+// QUESTION #05
+
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+orderTotal=110
+
+
+
+
+// QUESTION #06
+
+
+var initialNumber = 10; 
+initialNumber = initialNumber + 5;
+initialNumber=15
+
+
+
+// CHAPTER #03 COMPLETED
+
+// CHAPTER #04
+
+
+// QUESTION #01
+
+var productCost = 3.45;
+
+
+
+
+// QUESTION #02
+
+var nameOfBand
+
+
+
+
+
+// QUESTION #03
+
+
+var thisNumber = 42;
+
+
+
+
+
+// QUESTION #04
+
+
+var firstName = "Nehal";
+var lastName = "Tariq";
+var fullName = firstName + lastName;
+
+
+
+
+
+// QUESTION #05
+
+// legal varable names in javascript
+
+// >myVariable
+// >thisVariable
+// >firstVarible
+// >leagal589
+// >lastVariable
+
+
+// illegal varable names in javascript
+
+// >5767illegal
+// >this is illegal 
+// >illegal-variable
+// >@ illegal
+// >illegal.variable
+
+
+// CHAPTER #04 COMPLETED
+
+
+
+// QUESTION #01
+
+// Name is (modulo) and symbol is ( % ) of arthemathic operater thats give you remainder that is one number is divided by another.
+
+
+// QUESTION #02
+
+
+num = 2
+
+
+// QUESTION #03
+
+
+var largeNum = 1000 * 2000;
+console.log("" + largeNum)
+
+
+
+
+
+// QUESTION #04
+
+
+var variable1 = 999;
+var variable2 = 666;
+var result = variable1 - variable2;
+console.log("" + result);
+
+
+
+
+
+
+// QUESTION #05
+
+
+var myRemainder;
+var dividend = 17;
+var divisor = 5;
+myRemainder = dividend % divisor;
+console.log("" + myRemainder)
+
+
+
+// QUESTION #06
+
+
+var num1 = 99;
+var num2 = 45;
+var result = num1 * num2;
+
+alert(" multiplication: " + result);
+
+
+
+
+
